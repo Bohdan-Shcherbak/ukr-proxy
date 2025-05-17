f942b3c3d8c131e85b8e6045356daa2f20ca9d59
Bohdan-Shcherbak <Bohdansherbak@gmail.com>
2025-05-17 20:03:43 +0300

    fix:result

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/proxy', async (req, res) => {
                }
           });
           const $ = cheerio.load(response.data);
-          const result = $('p.cont_p').toArray().map(el => $(el.textContent).html());
+          const result = $('p.cont_p').toArray().map(el => $(el.textContent));
           res.send(result);
      } catch (error) {
           res.status(500).send('Помилка під час запиту');
